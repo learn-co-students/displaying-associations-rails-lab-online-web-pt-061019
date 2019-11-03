5158f1b3b8c1e63d05c43e6ecc4049080351b80d
scrabill <shannon.crabill@gmail.com>
2019-11-02 23:34:55 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+brit = Artist.create(name: "Britany Spears")
+outkast = Artist.create(name: "Outkast")
+
+toxic = Song.create(title: "Toxic", artist_id: 1)
+lucky = Song.create(title: "Lucky", artist_id: 1)
+crazy = Song.create(title: "You Drive Me Crazy", artist_id: 1)
+
+bombs = Song.create(title: "Bombs over Bahgdad", artist_id: 2)
