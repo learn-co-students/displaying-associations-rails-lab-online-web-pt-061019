2225d63b62e2b90445fd154d0138b22740ea77d7
maasa333 <konomaasa@gmail.com>
2019-11-03 16:57:37 -0800

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+teacher = Artist.create(name: "Teacher")
+abc = teacher.songs.create(title: "ABC")
